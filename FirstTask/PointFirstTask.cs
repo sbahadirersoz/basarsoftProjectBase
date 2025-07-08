4dbfd6fac90afe75c558811b385016b0da6ac30a
Semih Bahadır <bnoktaersoz@gmail.com>
2025-07-08 20:31:14 +0300

    Revert "Ilk Günün Taski"
    This reverts commit be7d067540dca710899d9187023a90537bfdb88f.

--- FirstTask/PointFirstTask.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace basarsoftProjectBase.FirstTask;
-
-public class PointFirstTask
-{
-    public string name { get; set; }
-    public long id { get; set; }
-    public long xAxis { get; set; }
-    public long yAxis { get; set; }
-}
